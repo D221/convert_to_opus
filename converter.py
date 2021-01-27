4f2822ee9e568785f2bbb64122f2eb31017fb571
D221 <b.pvilavicius@gmail.com>
2021-01-28 01:25:08 +0200

    fixed some issues on non windows

--- converter.py
+++ converter.py
@@ -5,13 +5,14 @@ import subprocess
 
 def convert(path, bitrate, original):
     commontypes = ('.flac', '.mp3', '.wav', '.m4a', '.aac')
-    subprocess.CREATE_NO_WINDOW # no cmd on windows
+    if os.name == 'nt':
+        subprocess.CREATE_NO_WINDOW # no cmd on windows
     for filename in os.listdir(path):
         if (filename.endswith(commontypes)):
             print("Converting "+filename)
             without_ext = os.path.splitext(filename)[0]
             os.system('ffmpeg -i "{0}" -loglevel panic -c:a libopus -b:a {1}k -vbr on "{2}.opus"'.format(
                 filename, bitrate, without_ext))
-            shutil.move('{0}\\{1}'.format(path, filename), original)
+            shutil.move('{0}/{1}'.format(path, filename), original)
         else:
             continue
